559d8c85fb2ecbbd1e9200c705e484683e6d5a13
RenweiMiao <mikemiao1995@hotmail.com>
2016-09-07 10:50:07 -0700

    Add files via upload
    Labwork 2

--- main.cpp
+++ main.cpp
@@ -1,45 +1,45 @@
 /* 
- * File:   main.cpp
- * Author: Renwei Miao
- * Created on September 7, 2016, 09:57 AM
- * Purpose:  Calculate cost of Stock Purchase
+   File:   main
+   Author: Renwei Miao  
+   Created on September,7 2016, 10;28 AM
+   Purpose:  
  */
 
-//Include system level libraries
-#include <iostream>   //Input/Output Libraries
-using namespace std;  //Libraries compiled under standard namespace
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
 
 //User Libraries
 
 //Global Constants
+const unsigned char PERCENT =100; //Global conversion
 
-//Functional Prototypes
 
-//Program execution begins here!
+//Function prototypes
+
+//Execution Begins Here!
 int main(int argc, char** argv) {
+    //Declaration of Variables
+    unsigned short cstSurv=16500,   //Number of customer surveyed
+                   cstEnrg,         //Energy drinkers
+                   cstCitr;         //Citrus energy drinkers
+    unsigned char  pEnergy=15,      //Percentage of energy drinkers
+                   pCitrus = 58;    //Percentage of citrus energy drinkers
     
-    //Declare and initialize variables
-    char s = ' '; //The space character
-    char c; //Character used to create the big c
-    
-    //Input value
-    cout <<"what character would you like to use to make a Big C,input\n";
-    cin >>c;
+    //Input values
+    cstEnrg =cstSurv * pEnergy / PERCENT;
+    cstCitr= cstEnrg*pCitrus/PERCENT;
    
-    //Process value-> Map inputs to outputs
-    
-    
-    //Display Output -> The Big C
-    cout<<s<<s<<c<<c<<c<<endl;
-    cout<<s<<c<<s<<s<<c<<endl;
-    cout<<c<<s<<s<<s<<s<<endl;
-    cout<<c<<s<<s<<s<<s<<endl;
-    cout<<c<<s<<s<<s<<s<<endl;
-    cout<<c<<s<<s<<s<<s<<endl;
-    cout<<c<<s<<s<<s<<s<<endl;
-    cout<<s<<c<<s<<s<<c<<endl;
-    cout<<s<<s<<c<<c<<c<<endl;
-   
+    //Process values -> Map inputs to Outputs
+    cout<< "The number of customers surveyed = "<< cstSurv<<endl;
+    cout<< "The percentage energy drinkers = "<< static_cast<int>(pEnergy)<<endl;
+    cout <<"The percentage citrus energy drinkers = "<<static_cast<int>(pCitrus)<<endl;
+    cout<< "The number of energy drinkers = "<<cstEnrg<<endl;
+    cout<< "The number of citrus energy drinkers = "<< cstCitr<<endl;
+    //Display Output
+
     //Exit Program
+    
+    
     return 0;
 }
